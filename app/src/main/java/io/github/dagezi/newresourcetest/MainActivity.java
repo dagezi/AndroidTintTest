d20d97f98355532dd60af396365204b5437133ad
Takeshi SASAKI <dagezi@gmail.com>
2015-12-18 22:43:51 +0900

    Support apiLevel 15.

--- app/src/main/java/io/github/dagezi/newresourcetest/MainActivity.java
+++ app/src/main/java/io/github/dagezi/newresourcetest/MainActivity.java
@@ -1,7 +1,10 @@
 package io.github.dagezi.newresourcetest;
 
+import android.graphics.drawable.Drawable;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -12,7 +15,7 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity {
 
     @Bind(R.id.iButton) ImageButton iButton;
-    @Bind(R.id.npButton) Button npButton;
+    @Bind(R.id.npButton) AppCompatButton npButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +23,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
+
+        npButton.setSupportBackgroundTintList(getResources().getColorStateList(R.color.button_colors));
+
+        Drawable roundRects = DrawableCompat.wrap(getResources().getDrawable(R.drawable.round_rect));
+        DrawableCompat.setTintList(roundRects, getResources().getColorStateList(R.color.button_colors));
+        iButton.setImageDrawable(roundRects);
     }
 
     public void disableIButton(View view) {
